3058514c6fafc98c9dbe383db14364efa607fd54
Pranjal Telmasre <pranjal.pce@gmail.com>
2026-06-11 23:35:17 +0530

    Add files via upload

---
+++ patternprinting.cpp
@@ -0,0 +1,33 @@
+//PATTERN PRINTING PROGRAM
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "\nPyramid Pattern:\n";
+
+    for (int i = 1; i <= rows; i++)
+    {
+        // Print spaces
+        for (int j = 1; j <= rows - i; j++)
+        {
+            cout << " ";
+        }
+
+        // Print stars
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
